0f5c6a15e50de223b482cba3db63df047aa1d10f
AnastasiiaBelianskaia <anastasiiabelianskaia@gmail.com>
2023-03-24 15:47:16 +0200

    Correct delete_users command

--- polls/management/commands/delete_users.py
+++ polls/management/commands/delete_users.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 
 User = get_user_model()
 
@@ -12,12 +12,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         users_id = kwargs['users_id']
-        superuser_list = []
-        for user in users_id:
-            superuser_list.append(
-                User.objects.filter(id=user, is_superuser=True).values_list('is_superuser', flat=True).first()
-            )
-        if True in superuser_list:
-            return self.stdout.write("You can't delete a superuser")
-        User.objects.filter(id__in=users_id).delete()
-        self.stdout.write("Specified users has been deleted from database!")
+
+        if User.objects.filter(pk__in=users_id, is_superuser=True).exists():
+            raise CommandError("You can't delete a superuser")
+        count, _ = User.objects.filter(id__in=users_id, is_superuser=False).delete()
+        self.stdout.write(f" {count} specified users has been deleted from database!")
